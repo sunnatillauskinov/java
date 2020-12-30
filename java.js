112e4475af27fd5574c31960a0cb0a0fab568892
User <qwert@hd-mail.com>
2020-12-30 11:52:41 +0500

    finished first part

---
+++ java.js
@@ -0,0 +1,32 @@
+console.log(1);
+console.log("mani ismim SUNNATILLA ");
+console.log(true);
+ console.log(false); 
+
+ var namee = 'sunnatilla'
+ var age  = 19;
+ var da = "da"
+ var yoshim = "yoshim"
+
+// console.log(namee);
+// console.log(age);
+// console.log(da);
+
+// console.log(" menig ismim " +  namee  + yoshim + age + 'da') ;
+
+var allinfo = " menig ismim " + namee + yoshim + age + 'da'
+
+
+// console.log(" menig ismim " + namee + yoshim + age + 'da');  
+
+console.log(6 + 4);
+
+var nick =prompt('Isminigizni kiriting');
+
+
+// console.log("nick");
+alert(" salom " + nick) 
+
+var num  = + prompt ('Yoshingizni kiriting')
+
+console.log(num);
