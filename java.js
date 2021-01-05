b6343aecacdf753b0ff7bba56e50eb93e22ea378
Your Name <you@example.com>
2021-01-05 18:36:54 +0500

    test

--- java.js
+++ java.js
@@ -1,32 +1,37 @@
-console.log(1);
-console.log("mani ismim SUNNATILLA ");
-console.log(true);
- console.log(false); 
-
- var namee = 'sunnatilla'
- var age  = 19;
- var da = "da"
- var yoshim = "yoshim"
-
-// console.log(namee);
-// console.log(age);
-// console.log(da);
-
-// console.log(" menig ismim " +  namee  + yoshim + age + 'da') ;
-
-var allinfo = " menig ismim " + namee + yoshim + age + 'da'
 
 
-// console.log(" menig ismim " + namee + yoshim + age + 'da');  
+var nick = prompt('Ismingiz nima')
+console.log( nick );
+alert("salom " + nick)
 
-console.log(6 + 4);
+var nun  = prompt ('Yoshingiz nechida');
+console.log( " Mening yoshi  " + nun  + " da");
+alert('oqish uchun zor yosh')
 
-var nick =prompt('Isminigizni kiriting');
 
 
-// console.log("nick");
-alert(" salom " + nick) 
 
-var num  = + prompt ('Yoshingizni kiriting')
 
-console.log(num);
+
+var nun = prompt('Misol :' + ' 5 * 5 + 25 = ?')
+console.log(" Misol 1: " + 'To`g`ri javob ' + ( 5 * 5 + 25) + ' sizning jovobinggiz ' + nun);
+
+var nun = prompt('Misol :' + ' 5 * 5 = ?')
+console.log(" Misol 2: " + 'To`g`ri javob ' + ( 5 * 5) + ' sizning jovobinggiz ' + nun);
+
+var nun = prompt ( 'Misol :' + ' 9 / 3 = ?')
+console.log(" Misol 3: " + 'To`g`ri javob ' + (9 / 3 ) + ' sizning jovobinggiz ' + nun );
+
+var nun = prompt ( 'Misol :' + ' 15 + 5 = ?')
+console.log(" Misol 4: " + 'To`g`ri javob ' + (15 + 5) + ' sizning jovobinggiz ' + nun );
+
+var nun = prompt ( 'Misol :' + ' 20 - 10 = ?')
+console.log(" Misol 5: " + 'To`g`ri javob ' + (20 - 10) + ' sizning jovobinggiz ' + nun );
+
+var nun = prompt ( 'Misol :' + ' 10 / 3 = ?')
+console.log(" Misol 6: " + 'To`g`ri javob ' + (10 / 3) + ' sizning jovobinggiz ' + nun );
+
+var  con = alert ('Javoblarni korish uchun consolni oching ') 
+
+
+        
